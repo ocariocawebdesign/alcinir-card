a5f1471b5d2407d5eafd200d4e1dbe6312f14d20
Carlos <ocariocawebdesign@gmail.com>
2022-10-27 16:14:40 -0300

    Novas alterações de margin

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -9,7 +9,7 @@ export default function Content() {
     <>
       <div
         id="wrapper"
-        className="container mt-2 animate__animated animate__fadeIn"
+        className="container mb-2 animate__animated animate__fadeIn"
       >
         <div className="row">
           <div id="block-1" className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -30,13 +30,13 @@ export default function Content() {
                         fontSize: "2.5em",
                         lineHeight: "1.25",
                         color: "rgba(40,40,43,0.6)",
-                        fontWeight: "300",
+                        fontWeight: "200",
                       }}
                     >
                       Alcinir Pessanha
                     </h1>
                     <p
-                      className="text-center text-uppercase"
+                      className="text-center"
                       style={{
                         letterSpacing: "-0.021875rem",
                         width: "100%",
@@ -51,8 +51,11 @@ export default function Content() {
                       <br /> 9° período de psicologia <br />
                       Atendimento online e presencial
                     </p>
+                    <hr className="solid"></hr>
                   </div>
-                  <hr className="solid"></hr>
+
+               
+                 
 
                   <div className="social-icons" >
                     <a href="https://api.whatsapp.com/send?phone=5521967530653&text=Ol%C3%A1!%20Quero%20informa%C3%A7%C3%B5es%20sobre%20Desenvolvimento%20de%20sites..." target="_blank" title="WhatsApp" alt="WhatsApp">
